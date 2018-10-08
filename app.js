85eac2b88fef0eebd6769d0e8f0184a78c95e6ca
akihitoChinen <croquette.picker@gmail.com>
2018-10-08 23:40:12 +0900

    init 3_3

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const url = require('url');
 const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
-const other_page = fs.readFileSync('./other.ejs', 'utf8');
+const login_page = fs.readFileSync('./login.ejs', 'utf8');
 const style_css = fs.readFileSync('./style.css', 'utf8');
+
+const max_num = 10;
+const filename = 'mydata.txt';
+var message_data;
+readFromFile(filename);
 
 var server = http.createServer(getFromClient);
 server.listen(3000);
@@ -18,19 +23,15 @@ function getFromClient(req,res){
         case '/':
             response_index(req, res);
             break;
-        case '/other':
-            response_other(req, res);
+        case '/login':
+            response_login(req, res);
             break;
         default:
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('no page...');
             break;
     }
-};
-
-var data = {
-    msg:'no message...'
-};
+}
 
 function response_index(req, res){
     if(req.method == 'POST'){
@@ -42,26 +43,49 @@ function response_index(req, res){
 
         req.on('end', () => {
             data = qs.parse(body);
-            setCookie('msg', data.msg, res);
+            addToData(data.id, data.msg, filename, req);
             write_index(req,res);
         });
      } else {
-         write_index(req,res);
+        write_index(req,res);
     }
 }
 
 function write_index(req, res) {
-    var msg = "※伝言を表示します。"
-    var cookie_data = getCookie('msg', req);
+    var msg = "※何かメッセージを書いてください。"
     var content = ejs.render(index_page, {
         title: "Index",
         content: msg,
-        data: data,
-        cookie_data:cookie_data,
+        data: message_data,
+        filename: 'data_item',
     });    
     res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
     res.write(content);
     res.end();
+}
+
+function readFromFile(fname) {
+    fs.readFile(fname, 'utf8', (err, data) => {
+        message_data = data.split('\n');
+    })
+}
+
+function addToData(id, msg, fname, req) {
+    var obj = {'id': id, 'msg': msg};
+    var obj_str = JSON.stringify(obj);
+    console.log('add data: ' + obj_str);
+    message_data.unshift(obj_str);
+    if (message_data.length > max_num) {
+        message_data.pop();
+    }
+    saveToFile(fname);
+}
+
+function saveToFile(fname) {
+    var data_str = message_data.join('\n');
+    fs.writeFile (fname, data_str, (err) => {
+        if (err) { throw err; }
+    });
 }
 
 function setCookie(key, value, res) {
@@ -81,36 +105,9 @@ function getCookie(key, req) {
     return '';
 }
 
-function response_other(req, res) {
-    var msg = "this is other page.";
-    // if(req.method == 'POST'){
-        var body = '';
-
-        req.on('data', (data) => {
-            body += data;
-        });
-
-        req.on('end', () => {
-            var post_data = qs.parse(body);
-            msg += 'you write ' + post_data.msg + '.';
-            var content = ejs.render(other_page, {
-                title: "Other",
-                content: msg,
-                data: data2,
-                filename:'data_item'
-            });
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(content);
-            res.end();
-        });
-    // } else {    
-    //     var msg = "nothing page....";
-    //     var content = ejs.render(other_page, {
-    //         title: "Other",
-    //         content: msg,
-    //     });    
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write(content);
-    //     res.end();
-    // }
+function response_login(req, res) {
+    var content = ejs.render(login_page, {});
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(content);
+    res.end();
 }
